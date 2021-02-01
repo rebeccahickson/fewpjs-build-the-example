eadf1b6aefa8316d4d9affa044bdd0e98511f0ed
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-02-01 11:28:00 -0500

    commit tree d1504d91dbcb9c2a3d442e640abde404a117e6ff

--- main.js
+++ main.js
@@ -1,20 +1,39 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = "♡";
+const FULL_HEART = "♥";
 
-// Your JavaScript code goes here!
+document
+  .querySelectorAll(".media-post")
+  .forEach((article) => article.addEventListener("click", handleClick));
 
-
-
+function handleClick(event) {
+  mimicServerCall()
+    .then((response) => {
+      if (event.target.className == "like-glyph") {
+        if (event.target.innerText == EMPTY_HEART) {
+          event.target.innerText = `${FULL_HEART}`;
+        } else if (event.target.innerText == FULL_HEART) {
+          event.target.innerText = `${EMPTY_HEART}`;
+        }
+      }
+    })
+    .catch((response) => {
+      document.getElementById("modal").classList.remove("hidden");
+      document.getElementById("modal-message").innerText = `${response}`;
+      setTimeout(function () {
+        document.getElementById("modal").classList.add("hidden");
+      }, 5000);
+    });
+}
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      let isRandomFailure = Math.random() < 0.2;
       if (isRandomFailure) {
         reject("Random server error. Try again.");
       } else {
